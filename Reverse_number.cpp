234c807236c6dc63920370a5673b8b6b2a8f9410
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-05-12 09:05:40 +0530

    Add files via upload

---
+++ Reverse_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int reverse_num(int n)
+{
+   int rev_num = 0;
+   while(n>0){
+   	int ld = n%10;
+   	rev_num = rev_num*10+ld;
+   	n=n/10;
+   }
+   return rev_num;
+}
+int main(){
+   int n;
+   cin>>n;
+   cout<< reverse_num(n);
+}
