aa75d5b1dd7728edb125db9bee6b008bc98adad8
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-05-14 12:23:41 +0530

    Add files via upload

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 void bubble_sort(int arr[],int n){
      for(int i=n-1;i>1;i--){
+     	int didSwap=0;
      	for(int j=0;j<=i-1;j++){
      		if(arr[j]>arr[j+1]){
      			int temp = arr[j+1];
      			arr[j+1]=arr[j];
      			arr[j] = temp;
-
+                didSwap = 1;
      		}
      	}
+     	if(didSwap == 0){
+     		break;
+     	}
+     	cout<<"run\n";
      }
 }
 int main(){
