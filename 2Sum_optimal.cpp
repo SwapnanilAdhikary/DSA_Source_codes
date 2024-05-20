cbc4712f7d8aa52186e68e3f5abaef2b1134836f
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-05-20 11:29:38 +0530

    Create 2Sum_optimal.cpp

---
+++ 2Sum_optimal.cpp
@@ -0,0 +1,12 @@
+int left = 0,right = n-1;
+sort(book.begin(),book.end());
+while(left<right){
+	int sum = book[left]+book[right];
+	if(sum == target){
+	   return "YES";
+	}
+	else if(sum<target)left++;
+	else right --;
+	}
+	return "NO";
+}
