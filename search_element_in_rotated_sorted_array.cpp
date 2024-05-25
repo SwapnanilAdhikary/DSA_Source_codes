d445f39b8ebcb0529719466331a476342043ab6f
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-05-25 11:03:13 +0530

    Create search_element_in_rotated_sorted_array.cpp

---
+++ search_element_in_rotated_sorted_array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int k) {
+        int n=arr.size();
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==k) return mid;
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=k && k<=arr[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(arr[mid]<=k && k<=arr[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        
+        }
+      return -1;  
+    }
+};
