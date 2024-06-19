a6ed69511d7ab58aac92b40f70056ce02f8f707d
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-06-19 12:18:40 +0530

    Create Diameter_of_Binary.cpp

---
+++ Diameter_of_Binary.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
+    
+private:
+    int height(TreeNode* root, int& diameter) {
+        if (!root) {
+            return 0;
+        }
+        int lh = height(root->left, diameter);
+        int rh = height(root->right, diameter);
+        diameter = max(diameter, lh + rh);
+        return 1 + max(lh, rh);
+    }
+};
