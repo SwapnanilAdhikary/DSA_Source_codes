2f01719690a39f2ad60892bbe21a783f029c5507
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-06-25 11:12:27 +0530

    Add files via upload

---
+++ graph_representation_optimal.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;    ///all this for undirected graph
+int main(){
+	int n,m;
+	cin>>n>>m;
+	vector<int> adj[n+1];
+	for(int i=0;i<m;i++){
+		int u,v;
+		cin>> u>>v;
+		adj[u].push_back(v);   //on the Uth index save v 
+		adj[v].push_back(u);   // on the Vth index save u
+	}
+	return 0;
+}
