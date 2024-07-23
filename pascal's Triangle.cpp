2eb9e5a0b5415f3ead41eb1e42d7ea282daa5d46
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-07-23 11:41:04 +0530

    Create pascal's Triangle.cpp

---
+++ pascal's Triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+vector<int> generateRow(int row){
+    long long ans =1;
+    vector<int> ansRow;
+    ansRow.push_back(1);
+    for(int col = 1;col<row;col++){
+        ans = ans*(row-col);
+        ans=ans/(col);
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=1;i<=numRows;i++){
+            ans.push_back(generateRow(i));
+        }
+        return ans;        
+    }
+};
