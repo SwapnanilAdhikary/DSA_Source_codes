4f7f8576ae5a985b051f88f9bfb4421c6ee7a6d6
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-09-16 08:20:29 +0530

    Create Minimum Time Difference.cpp

---
+++ Minimum Time Difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        vector<int> minutes(timePoints.size());
+        for(int i=0;i<timePoints.size();i++){
+            int h = stoi(timePoints[i].substr(0,2));
+            int m = stoi(timePoints[i].substr(3));
+            minutes[i] = h * 60 + m;
+        }
+        sort(minutes.begin(),minutes.end());
+
+        int minDiff = INT_MAX;
+        for(int i=0;i<minutes.size() - 1; i++){
+            minDiff = min(minDiff,minutes[i+1]-minutes[i]);
+        }
+        minDiff = min(minDiff,24*60-minutes.back()+minutes.front());
+
+        return minDiff;
+    }
+};
