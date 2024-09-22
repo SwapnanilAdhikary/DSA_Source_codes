55b06fc6077bbee551c12adee61462903201346e
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-09-22 09:56:03 +0530

    Create Best time to buy and sell stocks.cpp

---
+++ Best time to buy and sell stocks.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int maxProfit = 0;
+        int n = prices.size();
+        for(int i = 0;i<n;i++){
+            int cost = prices[i] - mini;
+            maxProfit = max(maxProfit,cost);
+            mini = min(mini,prices[i]);
+        }
+        return maxProfit;
+    }
+};
