1386375b0db6f93ccd40c8f67e9c43ee8c1452ee
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-09-24 22:17:55 +0530

    Create Best time to Buy and Sell Easy.cpp

---
+++ Best time to Buy and Sell Easy.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int profit = 0;
+        for(int i=0;i<prices.size();i++){
+            int cost = prices[i] - mini;
+            profit = max(profit,cost);
+            mini = min(mini,prices[i]);
+        }
+        return profit;
+    }
+};
