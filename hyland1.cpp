70db66092573ec5ed49ef92242b9a5d039ba7762
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2025-03-21 15:49:04 +0530

    Update hyland1.cpp

--- hyland1.cpp
+++ hyland1.cpp
@@ -2,32 +2,53 @@
 using namespace std;
 //anagrams if sorted words are same
 
-bool isAnagram(string a,string b){
+bool isAnagram(string a, string b){
     sort(a.begin(),a.end());
     sort(b.begin(),b.end());
-    return a == b;
+    return a==b;
 }
-vector<vector<string>> Answer(vector<string> product, vector<string> query) {
-    int n = product.size();
-    int m = query.size();
+vector<vector<string>>Answer_Brute(vector<string> product,vector<string> query){
     vector<vector<string>> ans;
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (isAnagram(product[i],query[j])) {
-                ans.push_back({product[i]}); // Store as vector<string>
+    for(int i=0;i<product.size();i++){
+        for(int j=0;j<query.size();j++){
+            if(isAnagram(product[i],query[j])){
+                ans.push_back({product[i]});
             }
         }
     }
-    
     return ans;
+
+}
+
+vector<vector<string>>Answer_optimised(vector<string> product,vector<string> query){
+    unordered_map<string,vector<string>> anagramGroup;
+    vector<vector<string>> ans;
+
+    for(const string &p:product){
+        string sortedP = p;
+        sort(sortedP.begin(),sortedP.end());
+        anagramGroup[sortedP].push_back(p);
+    }
+
+    for(const string &q:query){
+        string sortedQ = q;
+        sort(sortedQ.begin(),sortedQ.end());
+
+        if(anagramGroup.count(sortedQ)){
+            ans.push_back(anagramGroup[sortedQ]);
+        }else{
+            ans.push_back({});
+        }
+    }
+    return ans;
+
 }
 
 int main() {
     vector<string> product = {"apple", "banana", "cherry", "date"};
     vector<string> query = {"ananab", "ated", "rapeg"};
     
-    vector<vector<string>> result = Answer(product, query);
+    vector<vector<string>> result = Answer_optimised(product, query);
     
     // Print the result
     for (const auto &vec : result) {
